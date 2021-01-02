1ede67a37bd5bae4ea96842af157db83ebcdbe31
heather johnsick <hjohnsick@yahoo.com>
2021-01-02 12:48:45 -0500

    added ui tests

---
+++ CreditCards.UITests/CreditCardWebAppShould.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.ObjectModel;
+
+namespace CreditCards.UITests
+{
+    public class CreditCardWebAppShould
+    {
+        private const string HomeUrl = "http://localhost:44108/";
+        private const string AboutUrl = "http://localhost:44108/Home/About";
+        private const string HomeTitle = "Home Page - Credit Cards";
+
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver()) //opens and closes chrome
+            {
+
+                driver.Navigate().GoToUrl(HomeUrl); //calls the browser to go to a specific page
+
+                DemoHelper.Pause();
+
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+
+                driver.Navigate().GoToUrl(HomeUrl);
+
+                DemoHelper.Pause();
+
+                driver.Navigate().Refresh();
+
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePageOnBack()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(HomeUrl);
+                IWebElement generationTokenElement =
+                    driver.FindElement(By.Id("GenerationToken"));
+                string initialToken = generationTokenElement.Text;
+                DemoHelper.Pause();
+                driver.Navigate().GoToUrl(AboutUrl);
+                DemoHelper.Pause();
+                driver.Navigate().Back();
+                DemoHelper.Pause();
+
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+
+                string reloadedToken = driver.FindElement(By.Id("GenerationToken")).Text;
+                Assert.NotEqual(initialToken, reloadedToken);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePageOnForward()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(AboutUrl);
+                DemoHelper.Pause();
+
+                driver.Navigate().GoToUrl(HomeUrl);
+                DemoHelper.Pause();
+                IWebElement generationTokenElement =
+                    driver.FindElement(By.Id("GenerationToken"));
+                string initialToken = generationTokenElement.Text;
+
+                driver.Navigate().Back();
+                DemoHelper.Pause();
+
+                driver.Navigate().Forward();
+                DemoHelper.Pause();
+
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+
+                string reloadedToken = driver.FindElement(By.Id("GenerationToken")).Text;
+                Assert.NotEqual(initialToken, reloadedToken);
+            }
+        }
+
+        [Fact]
+        public void DisplayProductsAndRates()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(HomeUrl);
+                DemoHelper.Pause();
+
+                ReadOnlyCollection<IWebElement> tableCells = driver.FindElements(By.TagName("td"));
+                
+                Assert.Equal("Easy Credit Card", tableCells[0].Text);
+                Assert.Equal("20% APR", tableCells[1].Text);
+                Assert.Equal("Silver Credit Card", tableCells[2].Text);
+                Assert.Equal("18% APR", tableCells[3].Text);
+                Assert.Equal("Gold Credit Card", tableCells[4].Text);
+            }
+        }
+    }
+}
+
---
+++ CreditCards.UITests/DemoHelper.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Threading;
+
+
+namespace CreditCards.UITests
+{
+    internal static class DemoHelper
+    {
+        public static void Pause(int secondsToPause = 3000)
+        {
+            Thread.Sleep(secondsToPause);
+        }
+    }
+}
